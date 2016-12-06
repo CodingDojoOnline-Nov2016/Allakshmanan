360908227e3b2975af002fd078416c6be521eaca
al.lakshmanan <anna.laks@gmail.com>
2016-12-06 13:17:54 -0800

    toss

--- codingdojo/python/flask/starterproj/server.py
+++ codingdojo/python/flask/starterproj/server.py
@@ -1,19 +1,42 @@
+import random
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    print "Hello2"
+    print "Hello Index"
     return render_template('index.html')
 
 @app.route('/roll', methods=["POST"])
 def roll():
-    print "Hello"
+    print "Hello after submit"
+    fname = request.form['name']
+    typeofdice = int(request.form['typeofdice'])
+    numberofdice = int(request.form['numberofdice'])
+    diceresult=0
+    result=0
+    flag = 0
+    for i in range(numberofdice):
+        while flag is 0:
+            dicetoss = random.random()
+            diceresult = int(dicetoss * 10)
+            if diceresult > 0 and diceresult <= typeofdice:
+                flag = 1
+                print diceresult
+        result = result + diceresult
+        flag=0
 
-    return redirect('/')
+    return render_template('roll.html',name=fname, results=result, td=typeofdice, nd=numberofdice )
 
+@app.route('/ninjas')
+def ninjas():
+    print "Hello after submit"
+    return render_template('yb.html')
 
-
+@app.route('/dojos/new')
+def dojos():
+    print "Hello after submit"
+    return render_template('index.html')
 
 app.run(debug=True)
